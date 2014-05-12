c7072845897393bd8b47f23166654fc175d21f55
Rob Taylor <roboncode@gmail.com>
2014-05-12 14:27:19 -0600

    broke out transition into a function updateDisplay in highlight.js

--- src/directives/highlight.js
+++ src/directives/highlight.js
@@ -23,20 +23,27 @@ angular.module('ux').directive('focusHighlight', function (focusManager) {
         return { top: Math.round(top), left: Math.round(left), width: box.width, height: box.height };
     }
 
+    function updateDisplay(el, activeElement) {
+        if (focusManager.canReceiveFocus(activeElement)) {
+            var rect = getOffsetRect(activeElement);
+            el.style.left = rect.left + "px";
+            el.style.top = rect.top + "px";
+            el.style.width = rect.width + "px";
+            el.style.height = rect.height + "px";
+        }
+    }
+
     return {
         scope: true,
         replace: true,
         link: function (scope, element, attrs) {
             var el = element[0];
-            document.addEventListener('focus', utils.throttle(function (evt) {
-                if (focusManager.canReceiveFocus(evt.target)) {
-                    var rect = getOffsetRect(evt.target);
-                    el.style.left = rect.left + 'px';
-                    el.style.top = rect.top + 'px';
-                    el.style.width = rect.width + 'px';
-                    el.style.height = rect.height + 'px';
-                }
-            }, true), 100);
+//                dispatcher.on("focusin", utils.throttle(function(evt) {
+//                    updateDisplay(el, evt.newTarget);
+//                }), 100);
+            document.addEventListener("focus", function (evt) {
+                updateDisplay(el, evt.target);
+            }, true);
         },
         template: '<div class="focus-highlight"></div>'
     };
