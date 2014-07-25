d6c396eb912b7ed10e786d34f4370836bfa3a5a1
Rob Taylor <roboncode@gmail.com>
2014-07-25 17:14:21 -0600

    fix to autofocus in element

--- src/directives/element.js
+++ src/directives/element.js
@@ -1,27 +1,25 @@
-/* global ux, utils */
+/* global angular, ux, utils */
 angular.module('ux').directive('focusElement', function (focusManager, focusQuery) {
     return {
-        link: function (scope, element, attr) {
-            var el = element[0],
-                timer;
-
+        scope: true,
+        link: function(scope, element, attr) {
+            var el = element[0], timer;
             if (focusQuery.isAutofocus(el)) {
-                var off = scope.$watch(function () {
+                var off = scope.$watch(function() {
                     off();
-                    focusManager.focus(el);
-                    timer = setInterval(function () {
+                    timer = setInterval(function() {
+                        focusManager.focus(el);
                         el.focus();
                         if (document.activeElement === el) {
                             clearInterval(timer);
                         }
                     }, 10);
                 });
+                scope.$on("$destroy", function() {
+                    clearInterval(timer);
+                });
             }
-
-            scope.$destroy(function () {
-               clearInterval(timer);
-            });
         }
     };
+});
 
-})
