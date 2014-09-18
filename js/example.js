955a342324a79ae35150e37386c04164347f6fe6
Rob Taylor <roboncode@gmail.com>
2014-09-18 17:27:58 -0600

    update to examples

--- js/example.js
+++ js/example.js
@@ -1,9 +1,20 @@
 /* global angular */
 var app = angular.module('app', ['fm']);
-app.directive('main', function ($log) {
+app.directive('main', function ($log, focusKeyboard) {
     return {
         link: function (scope) {
             scope.log = $log.log;
+
+            scope.arrowKeysEnabled = false;
+            scope.toggleArrowKeys = function () {
+                if (scope.arrowKeysEnabled) {
+                    focusKeyboard.disableArrowKeys();
+                } else {
+                    focusKeyboard.enableArrowKeys();
+                }
+
+                scope.arrowKeysEnabled = !scope.arrowKeysEnabled;
+            }
         }
     };
 });
